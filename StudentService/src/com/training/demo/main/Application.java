0e22b1bd9947f09864bea630a6381c903041dc39
srikar kandalam <kvps148@gmail.com>
2021-10-07 14:44:35 +0530

    Student Service !

---
+++ StudentService/src/com/training/demo/main/Application.java
@@ -0,0 +1,12 @@
+package com.training.demo.main;
+
+import com.training.demo.Student;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		Student student1=new Student(101,"rakesh",88);
+		System.out.println(student1.getMarkScored());
+	}
+
+}
