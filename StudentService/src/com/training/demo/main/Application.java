0300a6960be98ddfcf00cca304dda1f9005b67db
srikar kandalam <kvps148@gmail.com>
2021-10-07 14:51:30 +0530

    Commited printing name

--- StudentService/src/com/training/demo/main/Application.java
+++ StudentService/src/com/training/demo/main/Application.java
@@ -7,6 +7,7 @@ public class Application {
 	public static void main(String[] args) {
 		Student student1=new Student(101,"rakesh",88);
 		System.out.println(student1.getMarkScored());
+		System.out.println(student1.getRollNumber());
 	}
 
 }
